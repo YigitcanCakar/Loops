d960ad1c24c1dcd51da8eb2236dd1b0369365d25
Yigitcan Cakar <yigitcanckr97@gmail.com>
2021-01-23 15:20:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //array-dizi
+            //lenght-eleman sayısı
+
+            string[] kurslar = new string[]  { "java", "python", "C#","go","android"};
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+        }
+    }
+}
